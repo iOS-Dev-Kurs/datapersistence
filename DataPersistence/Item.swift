56b346a0bdb02cfae0f9ee825555c46f5b6c89f1
Arthur Heimbrecht <heimbrecht@stud.uni-heidelberg.de>
2016-06-13 13:22:15 +0200

    created AddControllers

--- DataPersistence/Item.swift
+++ DataPersistence/Item.swift
@@ -11,9 +11,16 @@ import CoreData
 
 class Item : NSManagedObject {
 	
+	@NSManaged var list: List
 	@NSManaged var name: String
 	@NSManaged var created: NSDate
 	@NSManaged var importance: Int
 	@NSManaged var done: Bool
 	
+	// Called when the object is created, i.e. inserted in a context
+	override func awakeFromInsert() {
+		super.awakeFromInsert()
+		// Set attribute value as initial value, bypassing undo mechanics and such
+		self.setPrimitiveValue(NSDate(), forKey: "created")
+	}
 }

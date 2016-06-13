c0471a1137f96c609899a3291c4217042686b2df
Arthur Heimbrecht <heimbrecht@stud.uni-heidelberg.de>
2016-06-13 13:19:00 +0200

    created AddControllers

--- DataPersistence/AddItemController.swift
+++ DataPersistence/AddItemController.swift
@@ -8,8 +8,40 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class AddItemController : UIViewController{
 	
+	/// The context to save changes to. Should be a disposable child context of the main context.
+	var context: NSManagedObjectContext!
+	var list: List?
 	
+	@IBOutlet var titleTextfield: UITextField!
+	@IBOutlet var importanceField: UISegmentedControl!
+	
+	
+	// MARK: User Interaction
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		switch segue.identifier! {
+			
+		case "saveItem":
+			
+			// Create list and set properties
+			let item = NSEntityDescription.insertNewObjectForEntityForName("To_Do_Thing", inManagedObjectContext: context) as! Item
+			item.name = titleTextfield.text ?? "Unnamed Item"
+			let importance = importanceField.selectedSegmentIndex
+			item.importance = importance ?? 0
+			// Save context
+			do {
+				try context.save()
+			} catch {
+				print("Failed saving context: \(error)")
+			}
+			
+		default:
+			break
+		}
+	}
+
 }
--- DataPersistence/AddListController.swift
+++ DataPersistence/AddListController.swift
@@ -8,8 +8,37 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class AddListController : UIViewController{
 	
+	/// The context to save changes to. Should be a disposable child context of the main context.
+	var context: NSManagedObjectContext!
 	
+	@IBOutlet var titleTextfield: UITextField!
+	
+	
+	// MARK: User Interaction
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		switch segue.identifier! {
+			
+		case "saveList":
+			
+			// Create list and set properties
+			let list = NSEntityDescription.insertNewObjectForEntityForName("To_Do_List", inManagedObjectContext: context) as! List
+			list.name = titleTextfield.text ?? "Unnamed List"
+			
+			// Save context
+			do {
+				try context.save()
+			} catch {
+				print("Failed saving context: \(error)")
+			}
+			
+		default:
+			break
+		}
+	}
+
 }

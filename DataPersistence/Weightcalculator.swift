a7b04a341cf3f9cbaab2e62cf7a38dfd80037aa2
Florian Franz <flofra90@icloud.com>
2016-06-20 00:36:12 +0200

    Adapted Todo list to GymTracker: New Features: *Excersise Screen *Calculation of Weights *Autoincrease of weights if done *List sorting for last done

---
+++ DataPersistence/Weightcalculator.swift
@@ -0,0 +1,9 @@
+//
+//  Weightcalculator.swift
+//  DataPersistence
+//
+//  Created by Florian M. on 14/06/16.
+//  Copyright © 2016 iOS Dev Kurs Universität Heidelberg. All rights reserved.
+//
+
+import Foundation
